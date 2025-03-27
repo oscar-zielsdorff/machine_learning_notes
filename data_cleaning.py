ec0e8b58cb39aa1fc87130bdfbe54a5b0ea184dc
Oscar Zielsdorff <oscarzielsorff@gmail.com>
2025-03-27 03:34:15 -0700

    data cleaning - scaling and normalization

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
-
-# read in employee data
-df = pd.read_csv('dirty_data.csv')
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats # for Box-Cox tranformation
+from mlxtend.preprocessing import minmax_scaling # minmax scaling
 
 # set seed for reproducibility
 np.random.seed(0)
+
+# read in employee data
+df = pd.read_csv('csv_files/dirty_data.csv')
 
 ## Missing Values ################################################
 # get number of missing values per col
@@ -35,4 +39,40 @@ df.fillna('Unknown')
 # axis=0 (default) copies across rows (downwards), 1 is across cols.
 df.bfill().fillna('Unknown')
 
-## Scaling & Normalization ##########################################
+## Scaling ###########################################################
+# Transorm data so that it fits within a certain range/scale.
+# such as 0-1 or 0-100. This helps compare variables equally. For
+# example, scaling Yen and USD (1 yen is worth less than 1 USD).
+# Or scaling height and weight.
+
+# generate 1000 random points from exponential distribution
+data = np.random.exponential(size=1000)
+
+# min-max scale the data between 0 and 1
+scaled_data = minmax_scaling(data, columns=[0])
+
+# plot both together
+fig, ax = plt.subplots(1, 2, figsize=(15,3)) # 1 row, 2 cols, size inch
+sns.histplot(data, ax=ax[0], kde=True, legend=False)
+ax[0].set_title('Original Data')
+sns.histplot(scaled_data, ax=ax[1], kde=True, legend=False)
+ax[1].set_title('Scaled Data')
+plt.savefig('plots/scaled.png') # The scaled data retains its shape
+
+## Normalization #####################################################
+# Change data so it can be described as a normal distribution.
+
+# Normal (Guassian) Distribution: Roughly equal amount of observations
+# fall above and below the mean. The mean and median are equal.
+# Also called a bell curve.
+
+# normalize data from prev section using boxcox
+normalized_data = stats.boxcox(data)
+
+# plot both together
+fig, ax = plt.subplots(1, 2, figsize=(15,3)) # 1 row, 2 cols, size inch
+sns.histplot(data, ax=ax[0], kde=True, legend=False)
+ax[0].set_title('Original Data')
+sns.histplot(normalized_data, ax=ax[1], kde=True, legend=False)
+ax[1].set_title('Normalized Data')
+plt.savefig('plots/normalized.png') # The shape of the data changed
